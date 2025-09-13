c61bbb51d6e489c91a655b6de8848c2f49522822
jopanzo <jpnzo2k12@gmail.com>
2025-09-13 10:22:23 -0400

    Updated column 2 - 4

--- src/components/ourServices.jsx
+++ src/components/ourServices.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import './css/ourServices.css'; // Import your CSS file
 import roof1 from '../assets/tempJPEG1.jpg';
+import roof2 from '../assets/roofrecover.jpg';
+import roof3 from '../assets/roofwork.jpg';
+import roof4 from '../assets/roofwork2.jpg';
 
 const OurServices = () => {
   return (
@@ -8,12 +11,27 @@ const OurServices = () => {
       <div className="grid-item">
         <img src={roof1} alt="Roof 1" className="service-image" />
         <div className="service-title">
-        Roof Maintenance
+          Roof Maintenance
         </div>
       </div>
-      <div className="grid-item">Preventative Care</div>
-      <div className="grid-item">Roof Replacement</div>
-      <div className="grid-item">Inspections</div>
+      <div className="grid-item">
+        <img src={roof2} alt="Roof 2" className="service-image" />
+        <div className="service-title">
+          Preventative Care
+        </div>
+      </div>
+      <div className="grid-item">
+        <img src={roof3} alt="Roof 3" className="service-image" />
+        <div className="service-title">
+          Roof Replacement
+        </div>
+      </div>
+      <div className="grid-item">
+        <img src={roof4} alt="Roof 4" className="service-image" />
+        <div className="service-title">
+          Inspections
+        </div>
+      </div>
     </div>
   );
 };
